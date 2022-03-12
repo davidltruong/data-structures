fef65d0d4f8736c5100139a5c138e3654f576cc1
David Truong <dtruong1992@gmail.com>
2022-03-12 11:18:23 -0800

    Complete Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,55 @@
 var BinarySearchTree = function(value) {
+  var binaryTree = Object.create(tree);
+  binaryTree.value = value;
+  binaryTree.left = null;
+  binaryTree.right = null;
+
+  return binaryTree;
 };
 
+var tree = {};
+
+tree.insert = function(value) {
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else if (value > this.value) {
+    if (this.right === null) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+tree.contains = function(value) {
+  console.log(value);
+  if (value === this.value) {
+    return true;
+  } else if (value < this.value) {
+    if (this.left !== null) {
+      return this.left.contains(value);
+    }
+  } else if (value > this.value) {
+    if (this.right !== null) {
+      return this.right.contains(value);
+    }
+  }
+  return false;
+};
+
+tree.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
